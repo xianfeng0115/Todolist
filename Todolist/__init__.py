a92cb331d17318ac671f6bd88a0b690a74816402
liuxianfeng <1091238794@qq.com>
2020-07-14 08:33:44 +0800

    更新

--- Todolist/__init__.py
+++ Todolist/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy # 导入扩展类
 import os,sys
-import click
+
+# SQLite URI compatible
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
 app=Flask(__name__)
 
 # 数据库配置
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型修改的监控
 app.config['SECRET_KEY'] = 'dev' 
 # 在扩展类实例化前加载配置
